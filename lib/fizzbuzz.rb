407c97fd9d8e8a902945e0efc0d41250f557dea5
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-02-09 10:49:23 +0000

    Written first test

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(number)
+    'fizz'
+end
+
+#(1..20).each do |n|
+        #if n % 5 == 0 && n % 3 == 0
+            #puts "Fizzbuzz"
+              #elsif n % 3 == 0
+               # puts "Fizz"
+              #elsif n % 5 == 0
+                #puts "Buzz"
+              #else 
+                #puts n 
+              #end
+          
+        #end
+    
